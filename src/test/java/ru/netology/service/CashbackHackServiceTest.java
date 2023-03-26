9a293b50287a6af5d017351119d719752d15c0a2
fps-git <107875518+fps-git@users.noreply.github.com>
2023-03-26 13:47:57 +0200

    CashBackHacker with JUnit4 and JUnitJupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,10 @@
 package ru.netology.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CashbackHackServiceTest {
+
+    //JUnit4
 
     @org.junit.Test
     public void shouldReturnAmountIfLessThan1000() {
@@ -40,5 +44,47 @@ public class CashbackHackServiceTest {
         int actual = service.remain(0);
 
         org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    //JUnitJupiter
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnAmountIfLessThan1000JJ() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 250;
+        int actual = service.remain(750);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnAmountIfMoreThan1000JJ() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 250;
+        int actual = service.remain(1750);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnZeroJJ() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldReturnAmountIfZeroJJ() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        assertEquals(expected, actual);
     }
 }
